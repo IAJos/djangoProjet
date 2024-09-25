d3179bb546596ba1ffdf95278c54ef4581c40352
CHIMI Jospin <chimikotieu@gmail.com>
2024-09-25 15:19:54 -0300

    startup django

---
+++ listings/models.py
@@ -0,0 +1,41 @@
+# listings/models.py
+
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class Band(models.Model):
+
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
+    def __str__(self):
+        return f'{self.name}'
+
+    name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5, null=True)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2021)], null=True)
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+
+
+class Listing(models.Model):
+
+    class ListingType(models.TextChoices):
+        RECORDS = 'R'
+        CLOTHING = 'C'
+        POSTERS = 'P'
+        MISC = 'M'
+
+    title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=100)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2021)], null=True)
+    type = models.fields.CharField(choices=ListingType.choices, max_length=5, null=True)
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
+
+
+# Create your models here.
